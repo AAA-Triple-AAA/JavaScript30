3ce05da539090eef35b556e03a5d0059bc3a16bc
Adrian Abella <fasttaco1@gmail.com>
2023-03-07 21:26:07 -0500

    Completed Day 5 -Flex Panel

---
+++ 05 - Flex Panel Gallery/script.js
@@ -0,0 +1,9 @@
+const panels = document.querySelectorAll(".panel");
+
+function toggleOpen() {
+    console.log(this);
+    this.classList.toggle("open");
+    this.classList.toggle("open-active");
+}
+
+panels.forEach((panel) => panel.addEventListener("click", toggleOpen));
